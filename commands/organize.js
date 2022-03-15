c17e85985d7721f24374710bee8950225d5eb054
unknown <khandelwalmohit006@gmail.com>
2022-03-15 22:49:02 +0530

    organize function complete

--- commands/organize.js
+++ commands/organize.js
@@ -1,3 +1,4 @@
+const { Console } = require("console");
 const fs=require("fs");
 const path=require("path");
 
@@ -17,7 +18,8 @@ let types ={
     ],
     images:[
         'png','jpg','jpeg'
-        ]
+        ],
+    code:['js','html','css','cpp','c','java']
 }
 
 
@@ -41,14 +43,55 @@ function organize(srcPath){
     // Basically reads the names of files present in directory
     let allFiles=fs.readdirSync(srcPath);
 
-    console.log(allFiles);
+    // console.log(allFiles);
     // 4. Traverse over all the files and classify them on the basis of their extensions
     for(let i=0;i<allFiles.length;i++){
-        let ext=allFiles[i].split('.')[1];
-        //  or we can use let ext=path.extname(allFiles[i]);
-        console.log(ext);
+
+        let fullPathOfFile=path.join(srcPath,allFiles[i]);
+        // console.log(fullPathOfFile);
+
+        let isFile=fs.lstatSync(fullPathOfFile).isFile();
+        if(isFile){
+
+            // 1. get extension name
+            let ext=allFiles[i].split('.')[1];
+            //  or we can use let ext=path.extname(allFiles[i]);
+            // console.log(ext);
+            
+            // 1.2 get folder name from extension
+            let folderName= getFolderName(ext);
+            console.log(folderName);
+            // 1.3 copy from src to destination 
+            copyFileToDest(srcPath,fullPathOfFile,folderName);
+
+        }
     }
 }
 
+function getFolderName(ext){
+    for(key in types){
+        for(let i=0;i<types[key].length;i++){
+            if(types[key][i]==ext){
+                return key;
+            }
+        }
+        // console.log(key);
+    }
+}
 
+function copyFileToDest(srcPath, fullPathOfFile,folderName){
+// create folder path of destination 
+let destFolderPath=path.join(srcPath,"organized_files",folderName);
+// make folder to keep file 
+// check folder if it exists otherwise make folder 
+if(!fs.existsSync(destFolderPath)){
+    fs.mkdirSync(destFolderPath);
+}
+
+// copy file from source folder to destination folder
+let fileName=path.basename(fullPathOfFile);
+let destFileName=path.join(destFolderPath,fileName);
+fs.copyFileSync(fullPathOfFile,destFileName);
+
+}
 organize();
